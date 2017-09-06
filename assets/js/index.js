f23ad651b0d713657e88364735df94e9f5507c0e
Bridger Herman <scooteeroo@gmail.com>
2017-09-05 19:57:15 -0500

    Add some pretty colors and margins

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,10 +4,10 @@ function init() {
   if (window.location.hash !== '#' + defaultPage) {
     window.location.hash = '#' + defaultPage;
   }
-  $('nav ul li a[href="' + window.location.hash + '"]').addClass('active');
+  $('nav ul li a[href="' + window.location.hash + '"]').parent().addClass('active');
   $(window).on('hashchange', function() {
-    $('nav ul li a').removeClass('active');
-    $('nav ul li a[href="' + window.location.hash + '"]').addClass('active');
+    $('nav ul li a').parent().removeClass('active');
+    $('nav ul li a[href="' + window.location.hash + '"]').parent().addClass('active');
   });
 }
 

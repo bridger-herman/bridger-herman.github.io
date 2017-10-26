bdff6de0ba40dfcd5e1cdde099821eb113faeb7b
Bridger Herman <scooteeroo@gmail.com>
2017-10-26 17:25:53 -0500

    Make things look nicer

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,14 +12,8 @@ function init() {
       window.location.hash + '"]').parent().addClass('active');
     }
   );
-  // console.log($('.fix-pic').width());
-  // $('.fix-pic')
   // Set up header items
   // TODO (Lost all functionality when I apparently neglected to push)
-}
-
-function fixPic() {
-  // <iframe frameborder="0" allowfullscreen="" class="flickr-embed-frame" webkitallowfullscreen="" mozallowfullscreen="" oallowfullscreen="" msallowfullscreen="" width="500" height="331" data-natural-width="500" data-natural-height="331" data-loaded="true" style="overflow: hidden; padding: 0px; margin: 0px; width: 500px; height: 331px; max-width: none;"></iframe>
 }
 
 document.onload = init();

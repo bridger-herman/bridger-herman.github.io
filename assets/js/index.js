c712cd11200a4f7ab71ce9c90ca273d89814c144
Bridger Herman <herma582@umn.edu>
2019-04-13 21:08:46 -0500

    Fun word gallery for about page

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,6 +8,9 @@ function init() {
     $(ttyObjs[i].tty).find('.shell-line').html(getShellLine(window.location.pathname));
   });
 
+  // Set up word gallery
+  initWordGalleries();
+
   // https://stackoverflow.com/a/18525368
   window.onkeydown = function(e) {
     if (e.keyCode == 32 && $(e.target).hasClass('tty')) {
---
+++ assets/js/wordGallery.js
@@ -0,0 +1,28 @@
+function initWordGalleries() {
+  $('.word-gallery').each((index, element) => {
+    var words = JSON.parse($(element).html().replace('\n', ''));
+    var previousIndex = 0;
+    $(element).html(words[0]);
+    setInterval(() => {
+      let wordIndex = previousIndex;
+      while (wordIndex == previousIndex) {
+        wordIndex = Math.floor(Math.random() * words.length);
+      }
+      previousIndex = wordIndex;
+      animateTransition(element, words[wordIndex]);
+    }, 4000);
+  });
+}
+
+function animateTransition(elementToChange, word) {
+  var letterIndex = 0;
+  $(elementToChange).html('');
+  let intervalId = setInterval(() => {
+    let oldHtml = $(elementToChange).html();
+    if (letterIndex < word.length) {
+      $(elementToChange).html(oldHtml + word[letterIndex++])
+    } else {
+      clearInterval(intervalId);
+    }
+  }, 60);
+}

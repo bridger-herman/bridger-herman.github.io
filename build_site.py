158d8a9fb5ac8b7b566b5da782f94ddd33a3cf48
Bridger Herman <herma582@umn.edu>
2023-09-26 11:22:15 -0500

    More tailwind for responsive header and autoreload for dev

--- build_site.py
+++ build_site.py
@@ -1,5 +1,10 @@
 import time
 import os
+import sys
+import asyncio
+import signal
+import websockets
+import threading
 import subprocess
 from pathlib import Path
 from PIL import Image
@@ -26,7 +31,9 @@ TAILWIND_CONFIG = Path('tailwind.config.js')
 
 EXCLUDE_NAMES = ['base.jinja']
 
-HOST_INFO = ('0.0.0.0', 8000)
+SERVER_HOST = '0.0.0.0'
+SERVER_PORT = 8000
+AUTORELOAD_PORT = 8001
 
 COPYRIGHT_BLOCK = '''
 <p>
@@ -37,19 +44,27 @@ COPYRIGHT_BLOCK = '''
 </p>
 '''.format(time.strftime('%b %d %Y'), time.strftime('%Y'))
 
+CONNECTIONS = set()
 
 class Watcher(FileSystemEventHandler):
+    '''
+    Rebuild the site when there are any changes to the file system
+    '''
     last_timestamp = time.time()
     def on_any_event(self, event):
         if time.time() - Watcher.last_timestamp > 1:
             print('Rebuilding site...')
             build_tailwind()
-            resize_images()
-            build_templates()
+            # resize_images()
+            build_templates(True)
             print('Finished building')
+            autoreload()
             Watcher.last_timestamp = time.time()
 
 class FileWatchers:
+    '''
+    Define which folders to watch in the filesystem to rebuild the website
+    '''
     def start(self):
         paths_to_watch = [TEMPLATE_DIR, ASSETS_DIR]
         observers = []
@@ -70,6 +85,9 @@ class FileWatchers:
         self.observers = None
 
 def resize_images():
+    '''
+    Resize all images to ensure that they are not too big
+    '''
     if not OUT_IMG_FOLDER.is_dir():
         OUT_IMG_FOLDER.mkdir()
 
@@ -100,13 +118,21 @@ def resize_images():
 
 
 def build_tailwind():
+    '''
+    Build the TailwindCSS (this step requires downloading the tailwind
+    standalone executable: https://tailwindcss.com/blog/standalone-cli)
+    '''
     # this step requires tailwindcss-* to be on PATH
     print('Building tailwind CSS... from ' + os.getcwd())
     tailwind_cli = 'tailwindcss-windows-x64.exe'
     result = subprocess.check_call([tailwind_cli, '-c', TAILWIND_CONFIG, '-i', TAILWIND_INPUT, '-o', TAILWIND_OUTPUT])
     print('  ...done')
 
-def build_templates():
+
+def build_templates(serve: bool):
+    '''
+    Build the templates for the actual pages
+    '''
     jinja_env = Environment(loader=FileSystemLoader(str(TEMPLATE_DIR)))
 
     template_list = list(map(lambda name: TEMPLATE_DIR.joinpath(name), filter(lambda
@@ -118,32 +144,64 @@ def build_templates():
         template = jinja_env.get_template(filepath.name)
         out_path = OUT_DIR.joinpath(filepath.stem + '.html')
         print('   ', template, '->', out_path)
-        out_html = template.render({'copyright_block': COPYRIGHT_BLOCK})
+        out_html = template.render({'copyright_block': COPYRIGHT_BLOCK, 'serve': serve})
         with open(out_path, 'w') as fout:
             fout.write(out_html)
 
     print('  ...done')
 
+async def register_browser(websocket):
+    CONNECTIONS.add(websocket)
+    try:
+        await websocket.wait_closed()
+    finally:
+        CONNECTIONS.remove(websocket)
+
+def autoreload():
+    print('Refreshing browsers')
+    websockets.broadcast(CONNECTIONS, 'Reload')
+
+async def run_autoreload_server():
+    async with websockets.serve(register_browser, SERVER_HOST, AUTORELOAD_PORT):
+        print('Starting autoreload server on ws://{}:{}'.format(SERVER_HOST, AUTORELOAD_PORT))
+        await asyncio.Future()  # run forever
+
 def main():
+    if 'help' in sys.argv:
+        print('usage: python3 build_site.py <serve>')
+        print('   ', 'serve: start a server for previewing changes and auto-reload')
+
     if not OUT_DIR.exists():
         OUT_DIR.mkdir()
 
-    httpd = HTTPServer(HOST_INFO, SimpleHTTPRequestHandler)
-
-    watchers = FileWatchers()
-    watchers.start()
+    serve = 'serve' in sys.argv
 
     build_tailwind()
     resize_images()
-    build_templates()
+    build_templates(serve)
 
-    print('Starting HTTP server at http://{}:{}, will rebuild whenever there\'s a change'.format(*HOST_INFO))
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        watchers.stop()
-        watchers.join()
-        print('Stopped')
+    if serve:
+        # simple server
+        httpd = HTTPServer((SERVER_HOST, SERVER_PORT), SimpleHTTPRequestHandler)
+        server_thread = threading.Thread(target = httpd.serve_forever, daemon=True)
+
+        # watch the file system
+        watchers = FileWatchers()
+        watchers.start()
+
+
+        print('Starting HTTP server at http://{}:{}, will rebuild whenever there\'s a change'.format(SERVER_HOST, SERVER_PORT))
+
+        try:
+            server_thread.start()
+            asyncio.run(run_autoreload_server())
+        except KeyboardInterrupt:
+            print('Shutdown called')
+            httpd.shutdown()
+            print('Server shutdown')
+            watchers.stop()
+            watchers.join()
+            print('Watchers stopped')
 
 if __name__ == '__main__':
     main()

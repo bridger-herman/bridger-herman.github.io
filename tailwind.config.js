48aa01d4d8d371ceeb2ffd209eb603d0d9696274
Bridger Herman <herma582@umn.edu>
2023-09-26 14:39:58 -0500

    Add font and responsive header mostly working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     'assets/**/*.{js,html,jinja}',
     'templates/**/*.jinja'
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'mono': ['"Fantasque Sans Mono"', 'ui-monospace', 'SFMono-Regular']
+    },
+    colors: {
+      'transparent': 'transparent',
+      'current': 'currentColor',
+      'white': '#ffffff',
+      'gray': colors.gray,
+      'zinc': colors.zinc,
+      'sunset-orange': '#da8970',
+      'sunset-blue': '#7986b4'
+    }
   },
   plugins: [],
 }
